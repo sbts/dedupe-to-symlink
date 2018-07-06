acbf25351d8551dd480a036b46f12f6d8d0f1f48
sbts <info@sbts.com.au>
2018-07-06 11:51:34 +0800

    Improve file list generation

--- dedup-new.sh
+++ dedup-new.sh
@@ -7,7 +7,7 @@
 # https://forums.plex.tv/t/plex-server-cache-over-310gb/274438/10
 #
 
-phototranscoder_path=$1
+read -rst5 phototranscoder_path < <( readlink -f "$1"; )
 
 last_hash=""
 last_file=""
@@ -22,13 +22,13 @@ first_file_group=""
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root!" 
-   exit 1
+#   exit 1
 fi
 
 
 if [ "$#" -ne 1 ]; then
     echo "Usage:"
-    echo "$@ /path/to/PhotoTranscoder"
+    echo "  ${0##*/} /path/to/PhotoTranscoder"
     exit 1
 fi
 
@@ -44,22 +44,26 @@ echo "Please ensure Plex is stopped and you have a backup before continuing."
 echo "This process can take hours to complete."
 echo ""
 echo "Press ENTER when ready."
-read
+#read
 
 
 echo "Generating list of files (part 1)..."
 tmpfilelistp1=$(mktemp)
-find "${phototranscoder_path}" -type f -printf "%C@\t" -exec md5sum "{}" \; > $tmpfilelistp1
+#find "${phototranscoder_path}" -type f -printf "%C@\t" -exec md5sum "{}" \; > $tmpfilelistp1
+while read -rst240 Ts Sum Name ; do
+    printf '%s %s %s\n' "$Sum" "$Ts" "$Name" >> $tmpfilelistp1
+done < <( find "${phototranscoder_path}" -type f -printf "%C@\t" -exec md5sum "{}" \; )
 
 
-echo "Generating list of files (part 2)..."
-tmpfilelistp2=$(mktemp)
-cat $tmpfilelistp1 | awk ' { print $2"\t"$1"\t"$3 } ' > $tmpfilelistp2
+#echo "Generating list of files (part 2)..."
+#tmpfilelistp2=$(mktemp)
+#cat $tmpfilelistp1 | awk ' { print $2"\t"$1"\t"$3 } ' > $tmpfilelistp2
 
 
 echo "Generating list of files (part 3)..."
 tmpfilelistp3=$(mktemp)
-cat $tmpfilelistp2 | sort > $tmpfilelistp3
+cat $tmpfilelistp1 | sort > $tmpfilelistp3
+#sort -o $tmpfilelistp3 $tmpfilelistp2
 
 
 echo "Symlinking duplicates to originals..."
@@ -91,3 +95,15 @@ do
     last_hash=$cur_hash
     last_file=$cur_file
 done < $tmpfilelistp3
+
+
+cat <<-EOF
+	tmpfilelistp1 = $tmpfilelistp1
+	tmpfilelistp2 = $tmpfilelistp2
+	tmpfilelistp3 = $tmpfilelistp3
+	EOF
+
+head -n5 $tmpfilelistp1
+#head -n5 $tmpfilelistp2
+head -n5 $tmpfilelistp3
+
